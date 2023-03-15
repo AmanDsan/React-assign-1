3a8bdbbe53e05c29b7e87ffc571da1b7c7cd8145
AmanDsan <126654741+AmanDsan@users.noreply.github.com>
2023-03-15 17:39:10 +0530

    Update toDoList.js

--- src/toDoList.js
+++ src/toDoList.js
@@ -2,35 +2,8 @@ import React, { useState } from 'react';
 import './App.css'
 
 function DoList() {
-  const [tasks, setTasks] = useState([]);
+    const [tasks, setTasks] = useState([]);
   const [newTask, setNewTask] = useState('');
-  const [count, setCount] = useState(0)
-
-  const countTask = ()=>{
-    setCount(count+1)
-  }
-
-  const decreaseTask = ()=>{
-    if(count>0){
-        setCount(count-1)
-    }
-  }
-
-  // const deecreaseTask2 = (index) => {
-  //   const newTasks = [...tasks];
-  //   newTasks[index].completed ? setCount(count + 1) : setCount(count -1)
-  // }
-  
-
-  const handleBoth1 = (index,task)=>{
-    decreaseTask();
-    handleComplete(index)
-  }
-
-  const handleBoth2 = (index)=>{
-    decreaseTask();
-    handleDelete(index)
-  }
 
   const handleSubmit = e => {
     e.preventDefault();
@@ -44,45 +17,36 @@ function DoList() {
     setTasks(newTasks);
   };
 
-  const handleDelete = index => {
+  const handleDelete = (index) => {
     const newTasks = [...tasks];
     newTasks.splice(index, 1);
     setTasks(newTasks);
   };
-
+  const pendingTasks = tasks.filter((task) => !task.completed);
   return (
-    <div>
-      <h1 style={{ position: 'sticky', top:0, backgroundColor: 'rgb(59, 53, 53)', width:'100%', 
-      textAlign: 'center'}}>Pending Tasks ({count})</h1>
-      
-      
+    <>
+      <h1 style={{paddingTop:10, position: 'sticky', top:0, backgroundColor: 'rgb(59, 53, 53)', width:'100%', textAlign: 'center'}}>Pending Tasks ({pendingTasks.length})</h1>
         {tasks.map((task, index) => (
-
           <li className='listItem'
             key={index}
-            style={{ textDecoration: task.completed ? 'line-through' : 'none' ,listStyleType: 'none'}}
+            style={{ textDecoration: task.completed ? 'line-through' : 'none', listStyleType :'none'}}
           >
-           <div style={{fontSize:20, overflowWrap: 'break-word', inlineSize: 176}}>{task.text}</div>
-            <div className='btnDiv'><button onClick={() => handleBoth1(index,task)}>Complete</button>
-            <button onClick={() => handleBoth2(index)}>Delete</button>
+            <div style={{fontSize:20, overflowWrap: 'break-word', inlineSize: 176}}>{task.text}</div>
+            <div>
+                <button onClick={() => handleComplete(index,task)}  className='btnDiv'>Complete</button>
+            <button onClick={() => handleDelete(index)} className='btnDiv'>Delete</button>
             </div>
-           
           </li>
-        
         ))}
-      
-
-      <form onSubmit={handleSubmit} style={{position: 'sticky', bottom:0, backgroundColor: 'rgb(59, 53, 53)', 
-      width:'100%', textAlign: 'center'}}>
+     <form onSubmit={handleSubmit} style={{position: 'sticky', bottom:0, backgroundColor: 'rgb(59, 53, 53)', width:'100%', textAlign: 'center'}}>
         <input
           type="text"
           value={newTask}
           onChange={e => setNewTask(e.target.value)}
         />
-        <button style={{margin : 10, padding:10, backgroundColor: 'green', border:'none',
-         borderRadius: 5, color: 'white'}} type="submit" onClick={countTask}>Submit</button>
+        <button style={{margin : 10, padding:10, backgroundColor: 'green', border:'none', borderRadius: 5, color: 'white'}} type="submit">Submit</button>
       </form>
-    </div>
+    </>
   );
 }
 
